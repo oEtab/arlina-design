e874f43aacedc502d15a41ab7cd13ae9fc57a078
melani.find96@gmail.com <melani.find96@gmail.com@f17e53d1-1a3b-cfba-96d2-677222e0e814>
2014-09-26 05:12:26 +0000

    2

--- photovie.js
+++ photovie.js
@@ -8,4 +8,4 @@ $('a[href$=bmp]:has(img)').lightBox();
 });
 
 //link
-document.getElementByclass("cpright").innerHTML="Design by <a href='http://arlinadesign.blogspot.com/' rel='dofollow' target='_blank' title='Arlina Design'>Arlina</a> Powered by <a href='http://www.blogger.com/' rel='nofollow' target='_blank' title='Blogger'>Blogger</a>"
+document.getElementById("cpright").innerHTML="Design by <a href='http://arlinadesign.blogspot.com/' rel='dofollow' target='_blank' title='Arlina Design'>Arlina</a> Powered by <a href='http://www.blogger.com/' rel='nofollow' target='_blank' title='Blogger'>Blogger</a>"
