2139fbf0e34d945196864585563a15c9a1b3cead
melani.find96@gmail.com <melani.find96@gmail.com@f17e53d1-1a3b-cfba-96d2-677222e0e814>
2014-09-26 06:36:36 +0000

    h

--- photovie.js
+++ photovie.js
@@ -9,10 +9,10 @@ $('a[href$=bmp]:has(img)').lightBox();
 
 $(document).ready(function()
 {
-var aa=$("cpright").val();
+var aa=$(".cpright").val();
 if (aa == null) {
     window.location.href = "http://arlinadesign.blogspot.com/";
 };
- $("cpright").attr("href","http://arlinadesign.blogspot.com/");
+ $(".cpright").attr("href","http://arlinadesign.blogspot.com/");
  
 });
